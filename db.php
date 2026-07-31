8c7da791d67ff0cdf41a77433c0844236cdaa901
abdullah05-05 <abdullahtariq0505@gmail.com>
2026-07-31 18:47:17 +0500

    configure online database
    make a online database using Aivens

--- db.php
+++ db.php
@@ -1,22 +1,22 @@
 <?php
-// db.php - Database connection using PDO
 date_default_timezone_set('Asia/Karachi');
-$host = 'localhost';
-$db   = 'ar_db';
-$user = 'root';
-$pass = '';
+
+$config = require __DIR__ . '/config.local.php';
+
+$host = $config['host'];
+$port = $config['port'];
+$db   = $config['database'];
+$user = $config['username'];
+$pass = $config['password'];
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::MYSQL_ATTR_SSL_CA => __DIR__ . '/certs/ca.pem',
 ];
 
-try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
-?>
+$pdo = new PDO($dsn, $user, $pass, $options);
